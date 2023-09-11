34a0266c399515c840c56bf42a7ff89340e63233
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-09-11 10:06:22 +0100

    Initial Commit

---
+++ mont_adventures/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mont_adventures project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mont_adventures.settings')
+
+application = get_asgi_application()
