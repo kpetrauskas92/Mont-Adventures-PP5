f7a46e00aa0de67584dbf9ff5324b840da53813a
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-09-18 12:11:12 +0100

    added trip models and tests

--- trip_packages/models.py
+++ trip_packages/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.db.models import JSONField
 
-# Create your models here.
+
+class Trips(models.Model):
+    """
+    Trips model for storing information about different travel packages.
+
+    - Stores the main image of the trip.
+    - Contains details like name, price, duration, and location.
+    - Maintains seasonal availability as an array of integers.
+    - Keeps track of the maximum group size and overall rating.
+    - Holds an integer value for the trip's difficulty level.
+    """
+    main_image = models.ImageField(upload_to='trip_packages/main_images/',
+                                   null=True, blank=True)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
+    duration = models.IntegerField(db_index=True)
+    location = models.CharField(max_length=100, db_index=True)
+    season = JSONField(db_index=True)
+    max_group_size = models.IntegerField(db_index=True)
+    overall_rating = models.DecimalField(max_digits=3, decimal_places=1,
+                                         null=True, blank=True, db_index=True)
+    difficulty = models.IntegerField(db_index=True)
+
+    @property
+    def difficulty_value(self):
+        return self.difficulty
+
+
+class TripImage(models.Model):
+    """
+    TripImage model for storing additional images related to a trip.
+
+    - Holds a ForeignKey to the associated Trips model.
+    - Stores additional images in a specified upload directory.
+    """
+    trips = models.ForeignKey(Trips,
+                              related_name='images',
+                              on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='trip_packages/additional_images/')
+
+
+class AvailableDate(models.Model):
+    """
+    AvailableDate model for storing available dates for a trip.
+
+    - Holds a ForeignKey to the associated Trips model.
+    - Stores the start and end date for the trip's availability.
+    - Maintains the maximum group size and the number of booked slots.
+    - Contains a Boolean field to indicate if the trip is available.
+    """
+    trips = models.ForeignKey(Trips, related_name='available_dates',
+                              on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    max_group_size = models.IntegerField()
+    booked_slots = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+
+    def remaining_slots(self):
+        return self.max_group_size - self.booked_slots
+
+    def __str__(self):
+        return f"{self.trips.name} - {self.start_date} to {self.end_date}"
---
+++ trip_packages/tests/test_models.py
@@ -0,0 +1,99 @@
+from django.test import TestCase
+from trip_packages.models import Trips, AvailableDate
+
+
+class TripsModelTest(TestCase):
+    """
+    Test cases for the Trips model.
+
+    - Validates Trip object creation and attribute assignment.
+    - Checks the functionality of the difficulty_value property.
+    """
+
+    def setUp(self):
+        """
+        Setup a sample Trips object for testing purposes.
+        """
+        self.trip = Trips.objects.create(
+            name="Sample Trip",
+            price="100.00",
+            duration=3,
+            location="Sample Location",
+            season=[1, 2, 12],  # January, February, December
+            max_group_size=5,
+            difficulty=2,
+            overall_rating="4.5"
+        )
+
+    def test_trip_creation(self):
+        """
+        Test if the Trip object is created
+        and attributes are assigned correctly.
+        """
+        self.assertEqual(self.trip.name, "Sample Trip")
+        self.assertEqual(self.trip.price, "100.00")
+        self.assertEqual(self.trip.duration, 3)
+        self.assertEqual(self.trip.location, "Sample Location")
+        self.assertEqual(self.trip.season, [1, 2, 12])
+        self.assertEqual(self.trip.max_group_size, 5)
+        self.assertEqual(self.trip.difficulty, 2)
+        self.assertEqual(self.trip.overall_rating, "4.5")
+
+    def test_difficulty_value_property(self):
+        """
+        Test if the difficulty_value property returns the correct value.
+        """
+        self.assertEqual(self.trip.difficulty_value, 2)
+
+
+class AvailableDateModelTest(TestCase):
+    """
+    Test cases for the AvailableDate model.
+
+    - Validates AvailableDate object creation and attribute assignment.
+    - Checks the functionality of the remaining_slots method.
+    """
+
+    def setUp(self):
+        """
+        Setup a sample Trips object and an AvailableDate
+        object for testing purposes.
+        """
+        self.trip = Trips.objects.create(
+            name="Sample Trip",
+            price="100.00",
+            duration=3,
+            location="Sample Location",
+            season=[1, 2, 12],
+            max_group_size=5,
+            difficulty=2,
+            overall_rating="4.5"
+        )
+        self.available_date = AvailableDate.objects.create(
+            trips=self.trip,
+            start_date="2023-01-01",
+            end_date="2023-01-03",
+            max_group_size=10,
+            booked_slots=3,
+            is_available=True
+        )
+
+    def test_available_date_creation(self):
+        """
+        Test if the AvailableDate object is created
+        and attributes are assigned correctly.
+        """
+        self.assertEqual(self.available_date.trips, self.trip)
+        self.assertEqual(self.available_date.start_date.strftime('%Y-%m-%d'),
+                         "2023-01-01")
+        self.assertEqual(self.available_date.end_date.strftime('%Y-%m-%d'),
+                         "2023-01-03")
+        self.assertEqual(self.available_date.max_group_size, 10)
+        self.assertEqual(self.available_date.booked_slots, 3)
+        self.assertEqual(self.available_date.is_available, True)
+
+    def test_remaining_slots_method(self):
+        """
+        Test if the remaining_slots method returns the correct value.
+        """
+        self.assertEqual(self.available_date.remaining_slots(), 7)
