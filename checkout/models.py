2510b00c4450a1075abb98358b0e53365f7b0894
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-09-28 21:27:03 +0100

    added checkout models and tests

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.db.models import Sum
+from django_countries.fields import CountryField
+from trip_packages.models import Trips, AvailableDate
+from profiles.models import UserProfile
+import uuid
 
-# Create your models here.
+
+class Order(models.Model):
+    """
+    Model to represent an order in the checkout process.
+    """
+
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    stripe_pid = models.CharField(max_length=254, null=False,
+                                  blank=False, default='')
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID.
+
+        Returns:
+            str: A unique order number.
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update the grand total each time a line item is added.
+
+        Updates both the 'order_total' and 'grand_total'
+        fields and saves the model.
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.grand_total = self.order_total
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    """
+    Model to represent an individual line item in an order.
+    """
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='lineitems')
+    trip = models.ForeignKey(Trips, null=False, blank=False,
+                             on_delete=models.CASCADE)
+    available_date = models.ForeignKey(AvailableDate, null=False, blank=False,
+                                       on_delete=models.CASCADE)
+    guests = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                         null=False, blank=False,
+                                         editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the line item total
+        and update the order total.
+
+        Calculates the 'lineitem_total' based on 'trip.price'
+        and 'guests' and saves the model.
+        """
+        self.lineitem_total = self.trip.price * self.guests
+        super().save(*args, **kwargs)
---
+++ checkout/tests/test_models.py
@@ -0,0 +1,88 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django_countries.fields import Country
+from checkout.models import Order, OrderLineItem
+from profiles.models import UserProfile
+from trip_packages.models import Trips, AvailableDate
+from datetime import date
+
+
+class CheckoutModelTests(TestCase):
+
+    def setUp(self):
+        """
+        Set up common test data and state across test methods.
+        """
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass'
+        )
+        self.user_profile = UserProfile.objects.get(user=self.user)
+        self.trip = Trips.objects.create(
+            name="Trip 1",
+            price=100,
+            duration=3,
+            location="Location 1",
+            season=[1, 2],
+            max_group_size=5,
+            overall_rating=4.5,
+            difficulty=2
+        )
+
+        self.available_date = AvailableDate.objects.create(
+            trips=self.trip,
+            start_date=date(2023, 1, 1),
+            end_date=date(2023, 1, 10),
+            max_group_size=5,
+            booked_slots=0,
+            is_available=True
+        )
+
+        self.order = Order.objects.create(
+            user_profile=self.user_profile,
+            first_name='John',
+            last_name='Doe',
+            email='john@example.com',
+            country='US',
+            stripe_pid='test_stripe_pid'
+        )
+
+        self.order_line_item = OrderLineItem.objects.create(
+            order=self.order,
+            trip=self.trip,
+            available_date=self.available_date,
+            guests=2
+        )
+
+    def test_order_model(self):
+        """
+        Test the Order model for correct data types and field values.
+
+        This test checks that an Order instance is correctly created and that
+        its fields contain the expected data types and values.
+        """
+        self.assertIsInstance(self.order, Order)
+        self.assertIsInstance(self.order.order_number, str)
+        self.assertIsNotNone(self.order.order_number)
+        self.assertNotEqual(self.order.order_number, "")
+
+        self.assertEqual(self.order.user_profile, self.user_profile)
+        self.assertEqual(self.order.first_name, 'John')
+        self.assertEqual(self.order.last_name, 'Doe')
+        self.assertEqual(self.order.email, 'john@example.com')
+        self.assertEqual(self.order.country, Country('US'))
+        self.assertEqual(self.order.stripe_pid, 'test_stripe_pid')
+
+    def test_order_line_item_model(self):
+        """
+        Test the OrderLineItem model for correct data types and field values.
+
+        This test checks that an OrderLineItem instance is correctly created
+        and that its fields contain the expected data types and values.
+        It also checks if the 'lineitem_total' is calculated correctly.
+        """
+        self.assertEqual(self.order_line_item.order, self.order)
+        self.assertEqual(self.order_line_item.trip, self.trip)
+        self.assertEqual(
+            self.order_line_item.available_date, self.available_date)
+        self.assertEqual(self.order_line_item.guests, 2)
+        self.assertEqual(self.order_line_item.lineitem_total, 200)
