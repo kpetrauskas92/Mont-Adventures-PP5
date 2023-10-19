052ed68531c3a5cb172eccbc445b01d5f043cad9
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-10-19 17:26:57 +0100

    updated home view for search funtionality

--- home/views.py
+++ home/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.db.models import Q
+from trip_packages.models import Trips
 
 
 def index(request):
     """ A view to return the index page """
     return render(request, 'index.html')
+
+
+def search_trips(request):
+    query = request.GET.get('q', '')
+
+    results = Trips.objects.none()
+
+    if len(query) >= 3:
+        results = Trips.objects.filter(
+            Q(name__icontains=query) | Q(location__icontains=query)
+        )
+
+    context = {
+        'results': results,
+        'query': query
+    }
+    return render(request, 'includes/search-results.html', context)
