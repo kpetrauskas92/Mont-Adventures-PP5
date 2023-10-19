4f574a58101b858314377b2e773587df2dd8a0d3
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-10-19 14:23:27 +0100

    updated trip packages urls

--- trip_packages/urls.py
+++ trip_packages/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import TripPackages, TripDetails, BookingDrawer
+from .views import (TripPackages,
+                    TripDetails,
+                    BookingDrawer,
+                    TripReviews,
+                    trip_review_form)
 
 
 urlpatterns = [
@@ -9,4 +13,10 @@ urlpatterns = [
     path('trip_details/<int:pk>/', TripDetails.as_view(), name='trip_details'),
     path('booking_drawer/<int:trip_id>/', BookingDrawer.as_view(),
          name='booking_drawer'),
+    path('trip_packages/trip_reviews/<int:trip_id>/', TripReviews.as_view(),
+         name='trip_reviews'),
+    path('trip_reviews/<int:trip_id>/<int:review_id>/',
+         trip_review_form, name='trip_review_form_with_id'),
+    path('trip_review_form/<int:trip_id>/', trip_review_form,
+         name='trip_review_form'),
 ]
