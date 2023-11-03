35db6c383b381b2088b36ec4b4a7339d6f188a9c
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-11-03 10:20:37 +0000

    added error handlers, footer form

--- home/views.py
+++ home/views.py
@@ -54,6 +54,14 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+def error_404(request, exception):
+    return render(request, 'includes/error_handlers/404.html', {}, status=404)
+
+
+def error_500(request):
+    return render(request, 'includes/error_handlers/500.html', {}, status=500)
+
+
 def search_trips(request):
     query = request.GET.get('q', '')
     if query:
--- mont_adventures/urls.py
+++ mont_adventures/urls.py
@@ -19,3 +19,6 @@ if settings.DEBUG:
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+handler404 = 'home.views.error_404'
+handler500 = 'home.views.error_500'
