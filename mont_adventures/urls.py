5a632a03a32aafc1e832b08d6369b640e3737413
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-11-05 03:05:57 +0000

    updated  robots.txt

--- mont_adventures/urls.py
+++ mont_adventures/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
-from django.views.static import serve
+from django.views.generic.base import TemplateView
 from home.sitemaps import (HomeSitemap,
                            MainTripPackagesSitemap,
                            IndividualTripSitemap,
@@ -28,10 +28,12 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
-    path('robots.txt', serve, {
-        'path': 'robots.txt',
-        'document_root': settings.STATIC_ROOT,
-    }),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt",
+                             content_type="text/plain"),
+    ),
+
 ]
 
 if settings.DEBUG:
