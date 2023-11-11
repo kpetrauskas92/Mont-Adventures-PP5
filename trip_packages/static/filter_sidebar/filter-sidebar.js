bc384daf3507bfe5302d92aa0234ddb8afb45bbc
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-11-11 01:36:41 +0000

    html validation issue fixed for trip packages

--- trip_packages/static/filter_sidebar/filter-sidebar.js
+++ trip_packages/static/filter_sidebar/filter-sidebar.js
@@ -1,7 +1,7 @@
 // Function to update all #max-price and #max-price-label elements
 function syncMaxPrice(value) {
-    var maxPriceInputs = document.querySelectorAll('#max-price');
-    var maxPriceLabels = document.querySelectorAll('#max-price-label');
+    var maxPriceInputs = document.querySelectorAll('.max-price');
+    var maxPriceLabels = document.querySelectorAll('.max-price-label');
     
     maxPriceInputs.forEach(function(input) {
         input.value = value;
@@ -14,7 +14,7 @@ function syncMaxPrice(value) {
 
 // Listen for changes on any #max-price element
 document.body.addEventListener('input', function(event) {
-    if (event.target.id === 'max-price') {
+    if (event.target.classList.contains('max-price')) {
         syncMaxPrice(event.target.value);
     }
 });
@@ -32,31 +32,38 @@ document.addEventListener('DOMContentLoaded', function() {
 // Listen for changes to checkboxes and price sliders in both the main form and mobile sidebar
 document.addEventListener("DOMContentLoaded", function() {
     const inputs = document.querySelectorAll("input[type='checkbox'], input[type='range']");
-    const resetButtons = document.querySelectorAll("#resetButton");
-    const defaultPrice = document.getElementById("max-price").getAttribute('max');
+    const resetButtons = document.querySelectorAll(".resetButton");
 
     inputs.forEach(input => {
         input.addEventListener("change", function() {
-            const anyCheckedOrAdjusted = Array.from(inputs).some(
-                input => (input.type === 'checkbox' && input.checked) || 
-                        (input.type === 'range' && input.value !== defaultPrice)
-            );
-            resetButtons.forEach(resetButton => {
-                resetButton.classList.toggle("hidden", !anyCheckedOrAdjusted);
-            });
+            // Find the closest form, or null if there isn't one
+            const form = input.closest('form');
+            
+            // Proceed only if the input is inside a form
+            if (form) {
+                const defaultPrice = form.querySelector(".max-price").getAttribute('max');
+                const anyCheckedOrAdjusted = Array.from(inputs).some(
+                    input => (input.type === 'checkbox' && input.checked) || 
+                            (input.type === 'range' && input.value !== defaultPrice)
+                );
+                resetButtons.forEach(resetButton => {
+                    resetButton.classList.toggle("hidden", !anyCheckedOrAdjusted);
+                });
+            }
         });
     });
 });
 
+
 // Reset all filters
 function resetFilters() {
     // Main filter form and mobile drawer form
-    const filterForms = document.querySelectorAll("#filterForm");
+    const filterForms = document.querySelectorAll("[id^='filterForm-']");
 
     filterForms.forEach(filterForm => {
         const checkboxes = filterForm.querySelectorAll("input[type='checkbox']");
         const priceSlider = filterForm.querySelector("input[type='range']");
-        const priceLabel = filterForm.querySelector("#max-price-label");
+        const priceLabel = filterForm.querySelector(".max-price-label");
 
         checkboxes.forEach(checkbox => {
             checkbox.checked = false;
@@ -70,7 +77,7 @@ function resetFilters() {
         priceLabel.innerText = '€' + defaultPrice;
 
         // Hide the reset button
-        const resetButton = filterForm.querySelector("#resetButton");
+        const resetButton = filterForm.querySelector(".resetButton");
         resetButton.classList.add("hidden");
     });
 
