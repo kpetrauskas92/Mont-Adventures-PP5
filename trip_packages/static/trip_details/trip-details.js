8345eec9fce60de0cbeb8ca4b9caa37d68f8d802
Karolis <118168899+kpetrauskas92@users.noreply.github.com>
2023-11-12 01:59:29 +0000

    refactored trip details js, updated checkout success and email

--- trip_packages/static/trip_details/trip-details.js
+++ trip_packages/static/trip_details/trip-details.js
@@ -1,58 +1,67 @@
-    // Function to activate and scroll to the Reviews tab
-    function activateReviewsTab() {
-        const reviewsTab = document.getElementById('reviewsTab', 'overviewTab');
-        if (reviewsTab) {
-            manageActiveTab(reviewsTab);
-            reviewsTab.click();
-            reviewsTab.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+// Function to smoothly scroll to an element
+function smoothScrollToElement(element, duration = 800) {
+    const startY = window.scrollY;
+    const endY = element.getBoundingClientRect().top + window.scrollY - 10;
+    const distance = endY - startY;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const nextScrollY = ease(timeElapsed, startY, distance, duration);
+
+        window.scrollTo(0, nextScrollY);
+
+        if (timeElapsed < duration) {
+            requestAnimationFrame(animation);
         }
     }
-    
-    // Function to manage tab active state
-    function manageActiveTab(tabElement) {
-        const tabs = document.querySelectorAll('.tab');
-        tabs.forEach(tab => tab.classList.remove('tab-active'));
-        tabElement.classList.add('tab-active');
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
     }
-    
-    // Initialize when the DOM is fully loaded
-    document.addEventListener("DOMContentLoaded", function() {
-        // Handling tab clicks to toggle active state
-        const tabs = document.querySelectorAll(".tab");
-        tabs.forEach(tab => {
-            tab.addEventListener("click", function() {
-                manageActiveTab(this);
-                document.body.setAttribute('data-initiating-tab', this.id);
-            });
+
+    requestAnimationFrame(animation);
+}
+
+// Function to manage tab active state and scroll to the tab
+function manageActiveTabAndScroll(tabElement) {
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => tab.classList.remove('tab-active'));
+    tabElement.classList.add('tab-active');
+
+    // Smooth scroll to the tab element itself
+    smoothScrollToElement(tabElement);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const overviewTab = document.getElementById('overviewTab');
+    const reviewsTab = document.getElementById('reviewsTab');
+    const reviewLink = document.getElementById('reviewLink');
+
+    overviewTab.addEventListener('click', function() {
+        manageActiveTabAndScroll(this);
+    });
+
+    reviewsTab.addEventListener('click', function() {
+        manageActiveTabAndScroll(this);
+    });
+
+    // Event listener for ReviewLink
+    if (reviewLink) {
+        reviewLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            manageActiveTabAndScroll(reviewsTab);
         });
-    
-        // Reviews link event
-        const ReviewLink = document.getElementById('ReviewLink');
-        if (ReviewLink) {
-            ReviewLink.addEventListener("click", function() {
-                activateReviewsTab();
-                document.body.setAttribute('data-initiating-tab', 'reviewsTab');
-            });
-        }
-    
-        // Check URL parameters for specific tab
-        const urlParams = new URLSearchParams(window.location.search);
-        const tab = urlParams.get('tab');
-        if (tab === 'reviews') {
-            activateReviewsTab();
-        }
-    });
-    
-    // htmx specific event after DOM swap
-    document.body.addEventListener('htmx:afterSwap', function(event) {
-        const initiatingTabId = document.body.getAttribute('data-initiating-tab');
-        if (initiatingTabId) {
-            const tabElement = document.getElementById(initiatingTabId);
-            if (tabElement) {
-                manageActiveTab(tabElement);
-                tabElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-            }
-            // Clear the attribute so it doesn't affect other swaps
-            document.body.removeAttribute('data-initiating-tab');
-        }
-    });
+    }
+
+    // Check for 'tab=reviews' URL parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('tab') === 'reviews') {
+        manageActiveTabAndScroll(reviewsTab);
+        reviewsTab.click();
+    }
+});
