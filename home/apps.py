b4e69ea794a15153ab509d0932f147f9a4c0c736
kpetrauskas92 <k.petrauskas92@gmail.com>
2025-08-29 21:35:02 +0100

    updated apps.py for generating dates

--- home/apps.py
+++ home/apps.py
@@ -6,18 +6,11 @@ class HomeConfig(AppConfig):
     name = "home"
 
     def ready(self):
-        if os.environ.get("AUTO_CREATE_SUPERUSER", "").lower() != "true":
+        if os.environ.get("RUN_GENERATE_DATES", "").lower() != "true":
             return
-        from django.contrib.auth import get_user_model
-        from django.db.utils import OperationalError, ProgrammingError, IntegrityError
+        from django.core import management
+        from django.db.utils import OperationalError, ProgrammingError
         try:
-            User = get_user_model()
-            username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-            email = os.environ.get("DJANGO_SUPERUSER_EMAIL") or ""
-            password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-            if not username or not password:
-                return
-            if not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(username=username, email=email, password=password)
-        except (OperationalError, ProgrammingError, IntegrityError):
+            management.call_command("generate_dates")
+        except (OperationalError, ProgrammingError):
             pass
